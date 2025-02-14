fcadc5c18cb7f585995d91d8ee30c462a7160097
Tapan Ghataliya <tapanghataliya@Tapans-MacBook-Air.local>
2025-02-14 16:04:36 +0530

    remove some extra code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -71,38 +71,3 @@ const CustomNavbar = () => {
 };
 
 export default CustomNavbar;
-
-// import styles from './Navbar.module.css';
-// import Logo from "../logo/Logo";
-
-// import { useNavigate } from "react-router-dom";
-// import { useSelector, useDispatch } from "react-redux";
-
-// function Navbar() {
-
-//     const navigate = useNavigate();
-//     const count = useSelector((state) => state.cart.totalItems);
-
-//     const handleClick = () => {
-//         navigate('/cart');
-//     }
-
-//     const handlLogo = () => {
-//         navigate('/home');
-//     }
-//     return (
-//         <div className={styles.main}>
-//             <Logo onClick={handlLogo} />
-//             <div onClick={handleClick} send="TEST">
-//                 <img className={styles.checkout} alt="checkout" src="https://profiles-fyi-shivam907.vercel.app/cart.svg" />
-//                 <span className={styles.count}>
-//                     <p>{count}</p>
-//                 </span>
-//             </div>
-
-//         </div>
-
-//     )
-// }
-
-// export default Navbar;
